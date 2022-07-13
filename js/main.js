f5cac480499121940b67cfb13e9f4aeb4045de0f
leozheng <l.zheng444@gmail.com>
2022-07-13 10:20:03 -0600

    finished

--- js/main.js
+++ js/main.js
@@ -1,13 +1,85 @@
 // Title
 
 // Set Canvas Size
-canvasSize(800, 600);
-
+canvasSize(600, 400);
+// Player Object
+let player = {
+  x: 300,
+  y: 200,
+  w: 25,
+  h: 25,
+  speed: 2
+}//blocks
+let blocks = initBlocks(50);
 // Main Draw Loop
 window.addEventListener("load", draw);
 
 function draw() {
-  console.log("hi");
-
+// logic
+  movePlayer();
+  eatBlocks();
+  //DRAW
+  background();
+  drawPlayer();
+  drawBlocks();
   requestAnimationFrame(draw);
+}
+
+//Helper functions
+function background(){
+  ctx.fillStyle = "black";
+  ctx.fillRect(0,0, cnv.width, cnv.height);
+}
+function drawPlayer(){
+  ctx.strokeStyle = "white"
+  ctx.strokeRect(player.x, player.y, player.w, player.h);
+}
+function movePlayer(){
+  if (keyPressed["ArrowUp"]) {
+    player.y += -player.speed;
+  } else if (keyPressed["ArrowDown"]) {
+    player.y += player.speed;
+  }
+  if (keyPressed["ArrowLeft"]) {
+    player.x += -player.speed;
+  } else if (keyPressed["ArrowRight"]) {
+    player.x += player.speed;
+  }
+}
+//initialize an array with n random blocks
+function initBlocks(n) {
+  let temp = [];
+  for (let num = 1; num <= n; num++){
+    temp.push(newRandomBlock());
+  }
+  return temp;
+}
+// return a random block
+function newRandomBlock() {
+  return {
+    x: Math.random() * cnv.width,
+    y: Math.random() * cnv.height,
+    w: 15,
+    h: 15
+  }
+}
+//draw all the blocks
+function drawBlocks(){
+  ctx.fillStyle = "green";
+  for (let i = 0; i< blocks.length; i++) {
+    drawBlock(blocks[i]);
+  }
+}
+// draw a single block
+function drawBlock(block){
+  ctx.fillRect(block.x, block.y, block.w, block.h)
+}
+//check if player ate any blocks
+function eatBlocks() {
+  for (let i = 0; i < blocks.length; i++) {
+    if (rectCollide(player, blocks[i])) {
+      blocks.splice(i, 1);
+      break;
+    }
+  }
 }
